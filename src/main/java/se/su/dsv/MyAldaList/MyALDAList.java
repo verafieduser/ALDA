c94bb8f9ae6b2ddb5ed7acd3eeae545aefb4b31f
verafied <verasolnygren@gmail.com>
2022-01-19 14:32:40 +0100

    changed more stuff

--- src/main/java/se/su/dsv/MyAldaList/MyALDAList.java
+++ src/main/java/se/su/dsv/MyAldaList/MyALDAList.java
@@ -1,54 +1,90 @@
+//Vera Sol Nygren, klny8594
+
 package se.su.dsv.MyAldaList;
 import java.util.Iterator;
 
 public class MyALDAList<E> implements ALDAList<E>{
 
-    Node<E> node;
+    Node<E> firstNode;
+    Node<E> lastNode;
     int size;
+    int modifications;
 
     public void add(E element){
-        if(node == null){
-            node = new Node<>(element);
+        if(firstNode == null){
+            firstNode = new Node<>(element);
         } else {
-            node.setNext(new Node<>(element));
+            Node<E> newNode = new Node<>(element);
+            lastNode.setNext(newNode);
+            lastNode = newNode;
+
         }
         size++;
+        modifications++;
     }
 
 	public void add(int index, E element){
         if(index < 0 || index > size){
             throw new IndexOutOfBoundsException();
-        } 
+        } else if(index == 0){
+            Node<E> newNode = new Node<>(element);
+            newNode.setNext(firstNode);
+            firstNode = newNode;
+            size++;
+            modifications++;
+            return;
+        }
+
         Node<E> leftNode = getNodeByIndex(index-1);
         Node<E> rightNode = leftNode.getNext();
         Node<E> newNode = new Node<>(element);
         newNode.setNext(rightNode);
         leftNode.shiftNext(newNode);
+        if(rightNode == null){
+            lastNode = newNode;
+        }
         size++;
+        modifications++;
     }
 
 	public E remove(int index){
-        if(index < 0 || index > size){
+        if(index < 0 || index >= size){
             throw new IndexOutOfBoundsException();
         } 
-        Node<E> leftNode = getNodeByIndex(index-1);
-        Node <E> nodeToBeRemoved = leftNode.getNext();
-        Node<E> rightNode = nodeToBeRemoved.getNext();
-        leftNode.shiftNext(rightNode);
-        return nodeToBeRemoved.getData();
+        Node<E> nodeToBeRemoved;
+        if(index > 1){
+            Node<E> leftNode = getNodeByIndex(index-1); 
+            nodeToBeRemoved = leftNode.getNext();
+            remove(nodeToBeRemoved, leftNode);
+        } else {
+            nodeToBeRemoved = firstNode;
+            removeFirst();
+        }
 
+        return nodeToBeRemoved.getData();
     }
 
 	public boolean remove(E element){
-        int index = find(element);
-        if(index == -1){
-            return false;
-        }
-
-        remove(index);
-        return true;
-
+        return find(element, true) != -1;
     }
+
+    private void remove(Node<E> current, Node<E> previous){
+        Node<E> next = current.getNext();
+        previous.shiftNext(next);
+        modifications++;
+        size--;
+        if(current == lastNode){
+            lastNode = previous;
+        }
+    }
+
+    private void removeFirst(){
+        Node<E> newFirstNode = firstNode.getNext();
+        firstNode = newFirstNode;
+        size--;
+        modifications++;
+    }
+
 
 	public E get(int index){
         return getNodeByIndex(index).getData();
@@ -58,7 +94,7 @@ public class MyALDAList<E> implements ALDAList<E>{
         if(index < 0 || index >= size()){
             throw new IndexOutOfBoundsException();
         }
-        Node<E> nodeToGet = node;
+        Node<E> nodeToGet = firstNode;
         for(int i = 0; i < index; i++){
             nodeToGet = nodeToGet.getNext();
             if(nodeToGet == null){
@@ -74,17 +110,30 @@ public class MyALDAList<E> implements ALDAList<E>{
     }
 
     private int find(E element){
-        if(node == null){
+        return find(element, false);
+    }
+
+    private int find(E element, boolean removeFoundElement){
+        if(firstNode == null){
             return -1;
         }
-        E foundNode = node.getData();
+        Node<E> foundNode = firstNode;
+        Node<E> previousNode = firstNode;
+        E foundNodeElement = firstNode.getData();
         int counter = 0;
-        while(foundNode != element){
-            foundNode = node.getNext().getData();
-            counter++;
+        while(foundNodeElement != element){
+            previousNode = foundNode;
+            foundNode = foundNode.getNext();
+
             if(foundNode == null){
                 return -1;
             }
+            
+            foundNodeElement = foundNode.getData();
+            counter++;
+        }
+        if(removeFoundElement){
+            remove(foundNode, previousNode);
         }
         return counter;
     }
@@ -94,8 +143,10 @@ public class MyALDAList<E> implements ALDAList<E>{
     }
 
 	public void clear(){
-        node = null;
+        firstNode = null;
+        lastNode = null;
         size = 0;
+        modifications++;
     }
 
 	public int size(){
@@ -104,14 +155,57 @@ public class MyALDAList<E> implements ALDAList<E>{
 
     @Override
     public Iterator<E> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator<E> {
+
+        private int expectedModifications = modifications;
+        private Node<E> currentNode = firstNode;
+        private Node<E> previousNode;
+        private boolean okToRemove = false;
+
+        @Override
+        public boolean hasNext() {
+            return currentNode != lastNode;
+        }
+        @Override
+        public E next() {
+            if(modifications != expectedModifications){
+                throw new java.util.ConcurrentModificationException();
+            }
+            if (!hasNext()){
+                throw new java.util.NoSuchElementException();
+            }
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+            okToRemove = true;
+            return previousNode.getData();
+        }
+
+        @Override
+        public void remove(){
+            if(modifications != expectedModifications){
+                throw new java.util.ConcurrentModificationException();
+            }
+            if (!okToRemove){
+                throw new IllegalStateException();
+            }
+            if(previousNode == null){
+                removeFirst();
+            } else {
+                MyALDAList.this.remove(currentNode, previousNode);
+            }
+
+            expectedModifications++;
+            okToRemove = false;
+        }
     }
 
     private class Node<E> {
 
-        E data;
-        Node<E> nextNode;
+        private E data;
+        private Node<E> nextNode;
 
         public Node(E data){
             this.data = data;
