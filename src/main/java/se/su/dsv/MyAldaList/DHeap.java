6ebd872a2eb97519a53775a26720634b0a5dd8cb
verafied <verasolnygren@gmail.com>
2022-02-09 11:05:35 +0100

    -

--- src/main/java/se/su/dsv/MyAldaList/DHeap.java
+++ src/main/java/se/su/dsv/MyAldaList/DHeap.java
@@ -16,163 +16,190 @@ package se.su.dsv.MyAldaList;
 //Throws UnderflowException as appropriate
 
 /**
-* Implements a binary heap.
-* Note that all "matching" is based on the compareTo method.
-* @author Mark Allen Weiss
-*/
-public class DHeap<AnyType extends Comparable<? super AnyType>>
-{
- /**
-  * Construct the binary heap.
-  */
- public DHeap( )
- {
-     this( DEFAULT_CAPACITY );
- }
+ * Implements a binary heap.
+ * Note that all "matching" is based on the compareTo method.
+ * 
+ * @author Mark Allen Weiss
+ */
+public class DHeap<AnyType extends Comparable<? super AnyType>> {
 
- /**
-  * Construct the binary heap.
-  * @param capacity the capacity of the binary heap.
-  */
- public DHeap( int capacity )
- {
-     currentSize = 0;
-     array = (AnyType[]) new Comparable[ capacity + 1 ];
- }
- 
- /**
-  * Construct the binary heap given an array of items.
-  */
- public DHeap( AnyType [ ] items )
- {
-         currentSize = items.length;
-         array = (AnyType[]) new Comparable[ ( currentSize + 2 ) * 11 / 10 ];
+    int childrenPerNode;
 
-         int i = 1;
-         for( AnyType item : items )
-             array[ i++ ] = item;
-         buildHeap( );
- }
+    /**
+     * Construct the d-heap. Default amount of children permitted is 2 (binary heap)
+     */
+    public DHeap() {
+        this(2);
+    }
 
- /**
-  * Insert into the priority queue, maintaining heap order.
-  * Duplicates are allowed.
-  * @param x the item to insert.
-  */
- public void insert( AnyType x )
- {
-     if( currentSize == array.length - 1 )
-         enlargeArray( array.length * 2 + 1 );
+    /**
+     * Construct the binary heap.
+     * 
+     * @param childrenPerNode how many children each node is permitted.
+     */
+    public DHeap(int childrenPerNode) {
+        if(childrenPerNode<2){
+            throw new IllegalArgumentException();
+        }
+        currentSize = 0;
+        this.childrenPerNode = childrenPerNode;
+        array = (AnyType[]) new Comparable[DEFAULT_CAPACITY];
+    }
 
-         // Percolate up
-     int hole = ++currentSize;
-     for( array[ 0 ] = x; x.compareTo( array[ hole / 2 ] ) < 0; hole /= 2 )
-         array[ hole ] = array[ hole / 2 ];
-     array[ hole ] = x;
- }
+    /**
+     * Construct the binary heap given an array of items.
+     */
+    public DHeap(AnyType[] items, int childrenPerNode) {
+        if(childrenPerNode<2){
+            throw new IllegalArgumentException();
+        }
+        currentSize = items.length;
+        array = (AnyType[]) new Comparable[(currentSize + childrenPerNode) * 11 / 10];
 
+        int i = 1;
+        for (AnyType item : items)
+            array[i++] = item;
+        buildHeap();
+    }
 
- private void enlargeArray( int newSize )
- {
-         AnyType [] old = array;
-         array = (AnyType []) new Comparable[ newSize ];
-         for( int i = 0; i < old.length; i++ )
-             array[ i ] = old[ i ];        
- }
- 
- /**
-  * Find the smallest item in the priority queue.
-  * @return the smallest item, or throw an UnderflowException if empty.
-  */
- public AnyType findMin( )
- {
-     if( isEmpty( ) )
-         throw new UnderflowException( );
-     return array[ 1 ];
- }
+    /**
+     * Insert into the priority queue, maintaining heap order.
+     * Duplicates are allowed.
+     * 
+     * @param x the item to insert.
+     */
+    public void insert(AnyType x) {
+        if (currentSize == array.length - 1)
+            enlargeArray(array.length * childrenPerNode + 1);
 
- /**
-  * Remove the smallest item from the priority queue.
-  * @return the smallest item, or throw an UnderflowException if empty.
-  */
- public AnyType deleteMin( )
- {
-     if( isEmpty( ) )
-         throw new UnderflowException( );
+        // Percolate up
+        int hole = ++currentSize;
+        for (array[0] = x; x.compareTo(array[hole / childrenPerNode]) < 0; hole /= childrenPerNode)
+            array[hole] = array[hole / childrenPerNode];
+        array[hole] = x;
+    }
 
-     AnyType minItem = findMin( );
-     array[ 1 ] = array[ currentSize-- ];
-     percolateDown( 1 );
+    private void enlargeArray(int newSize) {
+        AnyType[] old = array;
+        array = (AnyType[]) new Comparable[newSize];
+        for (int i = 0; i < old.length; i++)
+            array[i] = old[i];
+    }
 
-     return minItem;
- }
+    /**
+     * Find the smallest item in the priority queue.
+     * 
+     * @return the smallest item, or throw an UnderflowException if empty.
+     */
+    public AnyType findMin() {
+        if (isEmpty())
+            throw new UnderflowException();
+        return array[1];
+    }
 
- /**
-  * Establish heap order property from an arbitrary
-  * arrangement of items. Runs in linear time.
-  */
- private void buildHeap( )
- {
-     for( int i = currentSize / 2; i > 0; i-- )
-         percolateDown( i );
- }
+    /**
+     * Remove the smallest item from the priority queue.
+     * 
+     * @return the smallest item, or throw an UnderflowException if empty.
+     */
+    public AnyType deleteMin() {
+        if (isEmpty())
+            throw new UnderflowException();
 
- /**
-  * Test if the priority queue is logically empty.
-  * @return true if empty, false otherwise.
-  */
- public boolean isEmpty( )
- {
-     return currentSize == 0;
- }
+        AnyType minItem = findMin();
+        array[1] = array[currentSize--];
+        percolateDown(1);
 
- /**
-  * Make the priority queue logically empty.
-  */
- public void makeEmpty( )
- {
-     currentSize = 0;
- }
+        return minItem;
+    }
 
- private static final int DEFAULT_CAPACITY = 10;
+    /**
+     * Establish heap order property from an arbitrary
+     * arrangement of items. Runs in linear time.
+     */
+    private void buildHeap() {
+        for (int i = currentSize / childrenPerNode; i > 0; i--)
+            percolateDown(i);
+    }
 
- private int currentSize;      // Number of elements in heap
- private AnyType [ ] array; // The heap array
+    /**
+     * Test if the priority queue is logically empty.
+     * 
+     * @return true if empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return currentSize == 0;
+    }
 
- /**
-  * Internal method to percolate down in the heap.
-  * @param hole the index at which the percolate begins.
-  */
- private void percolateDown( int hole )
- {
-     int child;
-     AnyType tmp = array[ hole ];
+    /**
+     * Make the priority queue logically empty.
+     */
+    public void makeEmpty() {
+        currentSize = 0;
+    }
 
-     for( ; hole * 2 <= currentSize; hole = child )
-     {
-         child = hole * 2;
-         if( child != currentSize &&
-                 array[ child + 1 ].compareTo( array[ child ] ) < 0 )
-             child++;
-         if( array[ child ].compareTo( tmp ) < 0 )
-             array[ hole ] = array[ child ];
-         else
-             break;
-     }
-     array[ hole ] = tmp;
- }
+    private static final int DEFAULT_CAPACITY = 10;
 
-     // Test program
- public static void main( String [ ] args )
- {
-     int numItems = 10000;
-     DHeap<Integer> h = new DHeap<>( );
-     int i = 37;
+    private int currentSize; // Number of elements in heap
+    private AnyType[] array; // The heap array
 
-     for( i = 37; i != 0; i = ( i + 37 ) % numItems )
-         h.insert( i );
-     for( i = 1; i < numItems; i++ )
-         if( h.deleteMin( ) != i )
-             System.out.println( "Oops! " + i );
- }
+    /**
+     * Internal method to percolate down in the heap.
+     * 
+     * @param hole the index at which the percolate begins.
+     */
+    private void percolateDown(int hole) {
+        int child;
+        AnyType tmp = array[hole];
+
+        for (; hole * childrenPerNode <= currentSize; hole = child) {
+            child = hole * childrenPerNode;
+            if (child != currentSize &&
+                    array[child + 1].compareTo(array[child]) < 0)
+                child++;
+            if (array[child].compareTo(tmp) < 0)
+                array[hole] = array[child];
+            else
+                break;
+        }
+        array[hole] = tmp;
+    }
+
+    // Test program
+    public static void main(String[] args) {
+        int numItems = 10000;
+        DHeap<Integer> h = new DHeap<>();
+        int i = 37;
+
+        for (i = 37; i != 0; i = (i + 37) % numItems)
+            h.insert(i);
+        for (i = 1; i < numItems; i++)
+            if (h.deleteMin() != i)
+                System.out.println("Oops! " + i);
+    }
+
+    public int parentIndex(int nodeIndex){
+        if(nodeIndex<2){
+            throw new IllegalArgumentException();
+        }
+        int parentIndex = nodeIndex/childrenPerNode;
+
+        return parentIndex;
+    }
+
+    public int firstChildIndex(int node){
+        if(node==0){
+            throw new IllegalArgumentException();
+        }
+
+        return 0;
+    }
+
+    public int size(){
+        return currentSize;
+    }
+
+    AnyType get(int index){
+        return array[index];
+    }
 }
